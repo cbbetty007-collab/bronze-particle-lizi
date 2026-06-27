827a559a61161b18e9adfc67c18d4ede02d14a8f
cbbetty007-collab <cbbetty007@gmail.com>
2026-06-27 16:37:10 +0800

    修复 GitHub Pages 部署配置

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/bronze-particle-relic-sxd/',
+  base: '/bronze-particle-lizi/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
